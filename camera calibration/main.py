ee48b8aaadece7eb37e04a5c5dd8717e5fb4b325
Kayalvizhi T <95753452+KayalvizhiT513@users.noreply.github.com>
2024-11-09 23:55:14 +0530

    Create main.py

---
+++ camera calibration/main.py
@@ -0,0 +1,70 @@
+import numpy as np
+import cv2 as cv
+import glob
+ 
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+print("Criteria:", criteria, cv.TERM_CRITERIA_EPS, cv.TERM_CRITERIA_MAX_ITER)
+ 
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*5,3), np.float32)
+# print("objp",objp)
+objp[:,:2] = np.mgrid[0:5,0:6].T.reshape(-1,2)
+# print("objp[:,:2]", objp)
+ 
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+ 
+images = glob.glob('*.jpg')
+print("Image", images)
+ 
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+ 
+    # Find the chess board corners
+    # to detect right-angle corners in an image
+    ret, corners = cv.findChessboardCorners(gray, (5,6), None)
+    # print("fname:",fname,"ret:",ret,"corners:",corners)
+ 
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+ 
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+ 
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        # cv.imshow('img', img)
+        # cv.waitKey(500)
+        filename = f"chessboard_corners_{fname}.png"
+        cv.imwrite(filename, img)
+
+# cv.destroyAllWindows()
+# distortion coefficients
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+img = cv.imread('left12.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+ 
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult12.png', dst)
+
+img = cv.imread('left08.jpg')
+h,  w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+# undistort
+mapx, mapy = cv.initUndistortRectifyMap(mtx, dist, None, newcameramtx, (w,h), 5)
+dst = cv.remap(img, mapx, mapy, cv.INTER_LINEAR)
+ 
+# crop the image
+x, y, w, h = roi
+dst = dst[y:y+h, x:x+w]
+cv.imwrite('calibresult13.png', dst)
